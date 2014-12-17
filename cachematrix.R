ea7511aaf8f742b01c3b334dedb1a0223715f1f3
irenemoral <imoral@eapsardenya.cat>
2014-12-17 10:58:26 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To solve this exercise I have followed the same reasoning
+# than the one used in the "Caching the Mean of a Vector" example
 
-## Write a short comment describing this function
+#The first function, makeCacheMatrix creates a special "matrix", 
+#which is really a list containing a function to 
+# 1.    set the value of the matrix
+# 2.	get the value of the matrix
+# 3.	set the value of the inverse matrix
+# 4.	get the value of the inverse matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+        
 }
 
-
-## Write a short comment describing this function
+## The following function calculates the inverse of the special "matrix" 
+# created with the above function. However, it first checks to see if 
+#the inverse matrix has already been calculated. If so, it gets the inverse
+# matrix from the cache and skips the computation. Otherwise, it calculates
+#the inverse matrix of the data and sets the value of the inverse matrix 
+#in the cache via the setinv function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+                ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
+
+#Testing that it runs
+
+matexample<-cbind(c(559,678,985), c(425,559,322), 524:526)
+
+matexample
+cacheSolve(makeCacheMatrix(matexample))
